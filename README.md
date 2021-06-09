e8b32bdc0fc81b5485a630548dad3b8aa025e5f2
tamaskis <tamas.a.kis@outlook.com>
2021-06-08 22:38:44 -0400

    Switched EXAMPLES to live script, updated README, updated function header, updated documentation.

--- README.md
+++ README.md
@@ -1,27 +1,51 @@
-# `line_intersection` [![View Intersection of Two Lines (line_intersection) on File Exchange](https://www.mathworks.com/matlabcentral/images/matlab-file-exchange.svg)](https://www.mathworks.com/matlabcentral/fileexchange/85428-intersection-of-two-lines-line_intersection)
+# `line_intersection`
+
 
 Finds the intersection of two lines.
 
 
+  
+  
 ## Syntax
+  
 
-`[x,y] = line_intersection([m1,b1],[m2,b2])`\
+
+`[x,y] = line_intersection([m1,b1],[m2,b2])`
+
+
+
+
+`[x,y] = line_intersection([x1,y1,m1],[m2,b2])`
+
+
+
+
+`[x,y] = line_intersection([m1,b1],[x2,y2,m2])`
+
+
+
+
 `[x,y] = line_intersection([x1,y1,m1],[x2,y2,m2])`
 
 
+  
+  
 ## Description
-
-`[x,y] = line_intersection([m1,b1],[m2,b2])` returns the intersection <a href="https://www.codecogs.com/eqnedit.php?latex=(x,y)" target="_blank"><img src="https://latex.codecogs.com/svg.latex?(x,y)" title="(x,y)" /></a> of two lines given in slope-intercept form:
-1. `[m1,b1]` specifies the first line in slope-intercept form: <img src="https://latex.codecogs.com/svg.latex?y=m_{1}x&plus;b_{1}" title="y=m_{1}x+b_{1}" />
-2. `[m2,b2]` specifies the second line in slope-intercept form: <img src="https://latex.codecogs.com/svg.latex?y=m_{2}x&plus;b_{2}" title="y=m_{2}x+b_{2}" />
-
-`[x,y] = line_intersection([x1,y1,m1],[x2,y2,m2])` returns the intersection <a href="https://www.codecogs.com/eqnedit.php?latex=(x,y)" target="_blank"><img src="https://latex.codecogs.com/svg.latex?(x,y)" title="(x,y)" /></a> of two lines given in point-slope form:
-1. `[x1,y1,m1]` specifies the first line in point-slope form: <img src="https://latex.codecogs.com/svg.latex?y-y_{1}=m_{1}\left(x-x_{1}\right)" title="y-y_{1}=m_{1}\left(x-x_{1}\right)" />
-2. `[x2,y2,m2]` specifies the second line in point-slope form: <img src="https://latex.codecogs.com/svg.latex?y-y_{2}=m_{2}\left(x-x_{2}\right)" title="y-y_{2}=m_{2}\left(x-x_{2}\right)" />
-
-The two lines may also be defined using different conventions, for example `line_intersection([m1,b1],[x2,y2,m2])`.
+  
 
 
-## Additional Documentation and Examples
+`[x,y] = line_intersection(__)` returns the intersection `[x,y]` of two lines, which can be defined as follows:
 
-See "DOCUMENTATION.pdf" for additional documentation and examples.
+
+
+   1.  `[m1,b1]` specifies the first line in slope-intercept form: <img src="https://latex.codecogs.com/svg.latex?\inline&space;y=m_1&space;x+b_1"/> 
+   1.  `[x1,y1,m1]` specifies the first line in point-slope form: <img src="https://latex.codecogs.com/svg.latex?\inline&space;y-y_1&space;=m_1&space;(x-x_1&space;)"/> 
+   1.  `[m2,b2]` specifies the second line in slope-intercept form: <img src="https://latex.codecogs.com/svg.latex?\inline&space;y=m_2&space;x+b_2"/> 
+   1.  `[x2,y2,m2]` specifies the first line in point-slope form: <img src="https://latex.codecogs.com/svg.latex?\inline&space;y-y_2&space;=m_2&space;(x-x_2&space;)"/> 
+
+  
+  
+## Examples
+
+   -  See "EXAMPLES.mlx" or the "Examples" tab on the File Exchange page for examples. 
+
--- line_intersection.m
+++ line_intersection.m
@@ -1,61 +1,68 @@
+%==========================================================================
+%
 % line_intersection  Finds the intersection of two lines.
 %
-%   [x,y] = line_intersection([m1,b1],[m2,b2]) returns the intersection 
-%   (x,y) of two lines given in slope-intercept form:
-%       (1) y1 = m1(x) + b1
-%       (2) y2 = m2(x) + b2
-%
-%   [x,y] = line_intersection([x1,y1,m1],[x2,y2,m2]) returns the 
-%   intersection (x,y) of two lines given in point-slope form:
-%       (1) y - y1 = m1(x - x1)
-%       (2) y - y2 = m2(x - x2)
-%
-%   The two lines may also be defined using different conventions, for
-%   example: [x,y] = line_intersection([m1,b1],[x2,y2,m2]).
+%   [x,y] = line_intersection([m1,b1],[m2,b2])
+%   [x,y] = line_intersection([x1,y1,m1],[m2,b2])
+%   [x,y] = line_intersection([m1,b1],[x2,y2,m2])
+%   [x,y] = line_intersection([x1,y1,m1],[x2,y2,m2])
 %
 % MATLAB Central File Exchange: https://www.mathworks.com/matlabcentral/fileexchange/85428-intersection-of-two-lines-line_intersection
 % GitHub: https://github.com/tamaskis/line_intersection-MATLAB
 %
-% See "DOCUMENTATION.pdf" for additional documentation and examples. 
-% Examples can also be found in EXAMPLES.m. Both of these files are 
-% included with the download.
+% See EXAMPLES.mlx for examples and "DOCUMENTATION.pdf" for additional 
+% documentation. Both of these files are included with the download.
 %
-% Copyright (c) 2021 Tamas Kis
-% Last Update: 2021-03-27
-
-
-
-%% FUNCTION
-
-% INPUT: line1 - parameters defining line, two options:
-%                 (1) [m1,b1]: slope-intercept form
-%                 (2) [x1,y1,m1]: point-slope form
-%        line2 - parameters defining line 2, two options:
-%                 (1) [m2,b2]: slope-intercept form
-%                 (2) [x2,y2,m2]: point-slope form
-% OUTPUT: [x,y] - intersection of lines 1 and 2
+% Copyright © 2021 Tamas Kis
+% Last Update: 2021-06-08
+%
+%--------------------------------------------------------------------------
+%
+% -------
+% INPUTS:
+% -------
+%   line1   - parameters defining line 1, two options:
+%               --> [m1,b1]: slope-intercept form
+%               --> [x1,y1,m1]: point-slope form
+% 	line2   - parameters defining line 2, two options:
+%               --> [m2,b2]: slope-intercept form
+%               --> [x2,y2,m2]: point-slope form
+%
+% --------
+% OUTPUTS:
+% --------
+%   x       - x-coordinate of the intersection of the two lines
+%   y       - y-coordinate of the intersection of the two lines
+%
+% -----
+% NOTE:
+% -----
+%   --> slope-intercept form: y=mx+b
+%   --> point-slope form: y-y0=m(x-x0)
+%
+%==========================================================================
 function [x,y] = line_intersection(line1,line2)
 
     % extracts/determines parameters for line 1
     if length(line1) == 2
-        x1 = 0; % 0 because we know the y-intercept b1
-        y1 = line1(2); % b1
-        m1 = line1(1); % m1
+        x1 = 0;         % 0 because we know the y-intercept b1
+        y1 = line1(2);  % b1
+        m1 = line1(1);  % m1
     else
-        x1 = line1(1); % directly from point-slope form
-        y1 = line1(2); % directly from point-slope form
-        m1 = line1(3); % directly from point-slope form
+        x1 = line1(1);  % directly from point-slope form
+        y1 = line1(2);  % directly from point-slope form
+        m1 = line1(3);  % directly from point-slope form
     end
     
     % extracts/determines parameters for line 2
     if length(line2) == 2
-        x2 = 0; % 0 because we know the y-intercept b2
-        y2 = line2(2); % b2
-        m2 = line2(1); % m2
+        x2 = 0;         % 0 because we know the y-intercept b2
+        y2 = line2(2);  % b2
+        m2 = line2(1);  % m2
     else
-        x2 = line2(1); % directly from point-slope form
-        y2 = line2(2); % directly from point-slope form
-        m2 = line2(3); % directly from point-slope form
+        x2 = line2(1);  % directly from point-slope form
+        y2 = line2(2);  % directly from point-slope form
+        m2 = line2(3);  % directly from point-slope form
     end
     
     % finds intersection point
